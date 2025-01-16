bf9d71e71ab516d5a3dd4ab81ce956e2e288f6f1
azevedojulio <54085370+azevedojulio@users.noreply.github.com>
2025-01-16 08:33:07 -0300

    adding front end next and first components

---
+++ short-url/app/components/DropDown.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export function DropdownMenuDemo() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Ferramentas</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Keyboard shortcuts
+            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ short-url/app/components/Header.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useState } from "react";
+
+const Header = () => {
+  const [url, setUrl] = useState<string>("");
+
+  const shortUrl = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:10000/shorten", {
+        url, // Envia a URL capturada
+      });
+      console.log("Resposta do servidor:", response.data);
+    } catch (error) {
+      console.error("Erro ao encurtar a URL:", error);
+    }
+  };
+
+  return (
+    <div className="container mx-auto my-auto px-4">
+      <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+        URL
+      </h2>
+      <form onSubmit={shortUrl}>
+        <Input
+          required
+          value={url}
+          onChange={(e) => setUrl(e.target.value)} // Atualiza o estado com o valor do campo
+          placeholder="Insira a URL aqui"
+        />
+        <Button type="submit" className="mt-2" variant="outline">
+          Encurtar
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Header;
