24073533752a236bc25a2fdb41c82901d3901f19
Julio Azevedo <azevedo.developer@gmail.com>
2025-01-31 14:55:47 -0300

    adding google ads

--- short-url/app/components/Header.tsx
+++ short-url/app/components/Header.tsx
@@ -7,24 +7,35 @@ import { useState } from "react";
 
 const Header = () => {
   const [url, setUrl] = useState<string>("");
+  const [shortedUrl, setShortedUrl] = useState<string>("");
 
   const shortUrl = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    alert(url)
+    alert(url);
     try {
       const response = await axios.post("http://localhost:10000/shorten", {
         fullUrl: url, // Envia a URL capturada
       });
-      console.log("Resposta do servidor:", response.data);
+      setShortedUrl(response.data.shortUrl);
     } catch (error) {
       console.error("Erro ao encurtar a URL:", error);
+    }
+  };
+
+  const copyToClipboard = async () => {
+    if (!shortUrl) return;
+
+    try {
+      await navigator.clipboard.writeText(shortedUrl);
+    } catch (error) {
+      console.error("Erro ao copiar o link: ", error);
     }
   };
 
   return (
     <div className="container mx-auto my-auto px-4">
       <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-        URL
+        Encurtador de URL
       </h2>
       <form onSubmit={shortUrl}>
         <Input
@@ -37,6 +48,23 @@ const Header = () => {
           Encurtar
         </Button>
       </form>
+      {shortedUrl && (
+        <>
+          <p className="mt-4">
+            URL encurtada:{" "}
+            <a href={shortedUrl} target="_blank">
+              {shortedUrl}
+            </a>
+          </p>
+          <Button 
+            onClick={copyToClipboard} 
+            className="mt-2" 
+            variant="outline"
+          >
+            Copiar Link
+          </Button>
+        </>
+      )}
     </div>
   );
 };
